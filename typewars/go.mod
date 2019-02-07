1bd5405114737c9e6fc8018ee509194ff7160fec
Gary Miller <gm@wx.io>
2019-02-07 15:02:49 +1100

    C

--- typewars/go.mod
+++ typewars/go.mod
@@ -1,3 +1,6 @@
 module typewars
 
-require github.com/golang/protobuf v1.2.0
+require (
+	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
+	github.com/golang/protobuf v1.2.0
+)
--- typewars/main.go
+++ typewars/main.go
@@ -1,17 +1,43 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"io/ioutil"
 	"os"
 	"typewars/pb"
 
+	"github.com/golang/glog"
 	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/protoc-gen-go/descriptor"
 )
 
 func main() {
-	redWing := pb.Xwing{
-		Pilot:      "Luke",
-		AstroDroid: "R2D2",
+	flag.Parse() // needed to make glog happy
+	fds := &descriptor.FileDescriptorSet{}
+	bytes, _ := ioutil.ReadAll(os.Stdin)
+	err := proto.Unmarshal(bytes, fds)
+	if err != nil {
+		glog.Fatalf("%v\n", err)
 	}
-	bytes, _ := proto.Marshal(&redWing)
-	os.Stdout.Write(bytes)
+	var script *pb.Script
+	for idx, fi := range fds.File {
+		fmt.Printf("%d proto '%s'\n", idx, fi.GetName())
+		if fi.GetName() == "anewhope.proto" {
+			ext, err := proto.GetExtension(fds.File[idx].Options, pb.E_File)
+			if err != nil {
+				glog.Fatalf("%v\n", err)
+			}
+			script = ext.(*pb.Script)
+		}
+	}
+	fmt.Printf("Props\n")
+	for i, prop := range script.Props {
+		fmt.Printf("Prop %d\n", i)
+		xwings := prop.GetXwings()
+		fmt.Printf("  XWings\n")
+		for i, xwing := range xwings.Xwingies {
+			fmt.Printf("  %d\t%+v\n", i, xwing)
+		}
+	}
 }
