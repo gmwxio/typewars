812f990248359919bdf25fe3478cc95e90b3d947
Gary Miller <gm@wx.io>
2019-02-07 14:49:01 +1100

    B

--- typewars/main.go
+++ typewars/main.go
@@ -1,17 +1,32 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"io/ioutil"
 	"os"
 	"typewars/pb"
 
+	"github.com/golang/glog"
 	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/protoc-gen-go/descriptor"
 )
 
 func main() {
-	redWing := pb.Xwing{
-		Pilot:      "Luke",
-		AstroDroid: "R2D2",
+	flag.Parse() // needed to make glog happy
+	fds := &descriptor.FileDescriptorSet{}
+	bytes, _ := ioutil.ReadAll(os.Stdin)
+	err := proto.Unmarshal(bytes, fds)
+	if err != nil {
+		glog.Fatalf("%v\n", err)
 	}
-	bytes, _ := proto.Marshal(&redWing)
-	os.Stdout.Write(bytes)
+	ext, err := proto.GetExtension(fds.File[1].Options, pb.E_File)
+	if err != nil {
+		glog.Fatalf("%v\n", err)
+	}
+	script := ext.(*pb.Script)
+	fmt.Printf("XWings\n")
+	for i, xwing := range script.GetXwing() {
+		fmt.Printf("%d\t%+v\n", i, xwing)
+	}
 }
